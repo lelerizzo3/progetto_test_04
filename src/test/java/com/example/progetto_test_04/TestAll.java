74ece1fcf135aebd9979ffbee3c608ae5be89a8c
DESKTOP-QB9SM4A\leler <lelerizzo3@gmail.com>
2022-12-07 18:28:25 +0100

    test all

---
+++ src/test/java/com/example/progetto_test_04/TestAll.java
@@ -0,0 +1,14 @@
+package com.example.progetto_test_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestAll {
+
+	@Test
+	void ciaooo() {
+		System.out.println("ciaooo");
+	}
+	
+}
